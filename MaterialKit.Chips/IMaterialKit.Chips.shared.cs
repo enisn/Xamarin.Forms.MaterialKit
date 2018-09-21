d5e3948c7b41b74e8d1f06da8b93a9c2ca6189c4
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2018-09-21 13:38:54 +0300

    Chips projects added

---
+++ MaterialKit.Chips/IMaterialKit.Chips.shared.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.MaterialKit.Chips
+{
+    public interface IMaterialKit.Chips
+    {
+    }
+}
---
+++ MaterialKit.Chips/MaterialKit.ChipsImplementation.android.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.MaterialKit.Chips
+{
+    /// <summary>
+    /// Interface for $safeprojectgroupname$
+    /// </summary>
+    public class MaterialKit.ChipsImplementation : IMaterialKit.Chips
+    {
+    }
+}
