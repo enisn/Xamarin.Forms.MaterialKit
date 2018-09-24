f4bcd9311b27ef7a070256acf44c8f0d59685a74
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2018-09-24 15:15:23 +0300

    ChipView and configurations

---
+++ MaterialKit.Chips/Shared/Configuration/GlobalSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Plugin.MaterialKit.Chips.Shared.Configuration
+{
+    public class GlobalSetting
+    {
+        public string ImageSource { get; set; }
+        public Color Color { get; set; }
+        public Color BackgroundColor { get; set; }
+    }
+}
---
+++ MaterialKit.Chips/Shared/Controls/ChipView.cs
@@ -0,0 +1,51 @@
+﻿using Plugin.MaterialKit.Chips.Shared.Configuration;
+using Plugin.MaterialKit.Core.Shared.Rendered;
+using Plugin.MaterialKit.Shared.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Plugin.MaterialKit.Chips.Shared.Controls
+{
+    public class ChipView : Frame
+    {
+        public static GlobalSetting GlobalSetting { get; private set; } = new GlobalSetting
+        {
+            ImageSource = "cancel_icon.png"
+        };
+        IconView imgCancel = new IconView { Source = GlobalSetting.ImageSource, FillColor = GlobalSetting.BackgroundColor.ToSurfaceColor().MultiplyAlpha(0.65) };
+        IconView imgIcon = new IconView { FillColor = GlobalSetting.BackgroundColor.ToSurfaceColor().MultiplyAlpha(0.65) };
+        Label lblContent = new Label { TextColor = GlobalSetting.BackgroundColor.ToSurfaceColor().MultiplyAlpha(0.65), HorizontalOptions = LayoutOptions.FillAndExpand };
+        public ChipView()
+        {
+            base.HasShadow = false;
+            this.Padding = new Thickness(5, 0);
+            UpdateIsDeletable();
+            this.Content = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    imgIcon,
+                    lblContent,
+                    imgCancel
+                }
+            };
+        }
+        public bool IsDeletable { get => (bool)GetValue(IsDeletableProperty); set => SetValue(IsDeletableProperty, value); }
+        public Command Command { get; set; }
+        public Command DeleteCommand { get; set; }
+        public object CommandParameter { get; set; }
+
+        private void UpdateIsDeletable()
+        {
+            imgCancel.IsVisible = IsDeletable;
+        }
+
+        public static readonly BindableProperty CommandProperty = 
+            BindableProperty.Create(nameof(Command),typeof(Command),typeof(ChipView),null,propertyChanged: (bo,ov,nv)=> (bo as ChipView).Command = (Command)nv)
+        public static readonly BindableProperty IsDeletableProperty =
+            BindableProperty.Create(nameof(IsDeletable), typeof(bool), typeof(ChipView), true, propertyChanged: (bo, ov, nv) => (bo as ChipView).UpdateIsDeletable());
+    }
+}
